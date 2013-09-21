c69ff85c054c5ab134bd370bba8d7012ab33fbd8
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-09-21 15:04:21 -0400

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackfitHackathon13::Application.config.secret_key_base = 'b55b643c555a74567f7d7135521b879e27e8958479cff6fe0d99e872c60c7127027719eb9bd5118f320bbf3b74b9cdf0998df5ddd52f0b24dd349c991dbb3d86'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HackfitHackathon13::Application.config.session_store :cookie_store, key: '_hackfit-hackathon-13_session'
