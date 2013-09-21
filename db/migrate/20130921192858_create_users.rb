b8082774573688aa4854b3cf892a0453f9d62fec
Nikola Savic <nikolasavic@gmail.com>
2013-09-21 18:45:09 -0400

    user migration

--- db/migrate/20130921192858_create_users.rb
+++ db/migrate/20130921192858_create_users.rb
@@ -1,6 +1,12 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+	  t.string :name
+	  t.string :email
+	  t.integer :weight
+	  t.integer :height
+	  t.string	:location
+	  t.string	:type
 
       t.timestamps
     end
