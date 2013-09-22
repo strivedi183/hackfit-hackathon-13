9eb29fca27b317744bb2343c716724a6ccc347d5
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-09-22 09:55:19 -0400

    Added seed stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+Course.delete_all
+Content.delete_all
+
+
